4677dcb74ba43ee64a39bbe8fcd896c48c051a0e
aseef0406 <Mohammed.aseef04@gmail.com>
2021-06-13 20:08:29 +0530

    Learned ArrayList

---
+++ src/com/aseef/jcfp/LearnArrayList.java
@@ -0,0 +1,20 @@
+package com.aseef.jcfp;
+
+import java.util.ArrayList;
+
+public class LearnArrayList {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		
+		for(int i=0;i<10;i++) {
+			al.add(i);
+		}
+		
+		System.out.println(al);
+		
+		
+	}
+
+}
