6e5026bff283419e87b9522dfea7a8104b349e81
Mohammed Aseef <mohammed.aseef04@gmail.com>
2021-06-14 21:58:47 +0530

    Learned ArrayList

--- src/com/aseef/jcfp/LearnArrayList.java
+++ src/com/aseef/jcfp/LearnArrayList.java
@@ -9,13 +9,22 @@ public class LearnArrayList {
 		//ArrayList<Object> name = new ArrayList<>();
 		ArrayList<Integer> al = new ArrayList<>();
 		
-		for(int i=0;i<10;i++) {
+		for(int i=0;i<115;i++) {
 			al.add(i);
 		}
 		
-		System.out.println(al);
+		System.out.println(al.get(0));
+		al.remove(0);//index
+		al.remove(Integer.parseInt(1));//object
 		
-		
+		Iterator<Integer> it = al.iterator();
+		while(it.hasNext()) {
+		      Integer i = it.next();
+		      System.out.println(i);	
+		      if(i < 10) {
+			it.remove();
+		      }
+		}		
 	}
 
 }
