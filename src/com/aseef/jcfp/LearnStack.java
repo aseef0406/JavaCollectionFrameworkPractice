4b070a4649d844a7ea06262eddec3070c36dd5be
aseef0406 <Mohammed.aseef04@gmail.com>
2021-06-20 19:50:08 +0530

    Learn Stack

--- src/com/aseef/jcfp/LearnStack.java
+++ src/com/aseef/jcfp/LearnStack.java
@@ -7,19 +7,20 @@ public class LearnStack {
 	public static void main(String[] args) {
 		
 		Stack<String> animals = new Stack<>();
+		//Stack is Data Structure which follows LIFO i.e., last in first out mechanism
 		
-		animals.push("Lion");
+		animals.push("Lion"); // will push/add the element in the end.
 		animals.push("Tiger");
 		animals.push("Cheetah");
 		animals.push("Panther");
 		
 		System.out.println(animals);
 		
-		System.out.println(animals.peek());
+		System.out.println(animals.peek()); // will give top of element
 		
-		animals.pop();
+		animals.pop(); // will remove top of element
 		
-		System.out.println(!animals.isEmpty());
+		System.out.println(!animals.isEmpty()); //isEmpty() will return true if stack is empty or false if not.
 		
 		System.out.println(animals);
 	}
