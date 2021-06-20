c715a08eeaf943d2b33ccc1e4c6540a8614f5939
aseef0406 <Mohammed.aseef04@gmail.com>
2021-06-20 19:15:53 +0530

    Learned ArrayList - addAll method

--- src/com/aseef/jcfp/LearnArrayList.java
+++ src/com/aseef/jcfp/LearnArrayList.java
@@ -1,6 +1,7 @@
 package com.aseef.jcfp;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class LearnArrayList {
 
@@ -8,23 +9,46 @@ public class LearnArrayList {
 		
 		//ArrayList<Object> name = new ArrayList<>();
 		ArrayList<Integer> al = new ArrayList<>();
+		ArrayList<Integer> nal = new ArrayList<>();
+		ArrayList<Integer> aal = new ArrayList<>();
 		
-		for(int i=0;i<115;i++) {
+		for(int i=0;i<15;i++) {
 			al.add(i);
 		}
 		
-		System.out.println(al.get(0));
+		nal=al;
+		
+		for(int i:al) {
+			aal.add(i);
+		}
+		
+		System.out.println(al);
 		al.remove(0);//index
-		al.remove(Integer.parseInt(1));//object
+		al.remove(Integer.valueOf(1));//object
 		
 		Iterator<Integer> it = al.iterator();
 		while(it.hasNext()) {
-		      Integer i = it.next();
-		      System.out.println(i);	
+		      Integer i = it.next();	
 		      if(i < 10) {
-			it.remove();
+		    	  it.remove();
 		      }
-		}		
+		}	
+		System.out.println(al);
+		
+		al.set(2, 100);
+		
+		System.out.println(al);
+		
+		System.out.println(al.contains(14));
+		
+		System.out.println(nal);
+		System.out.println(aal);
+		
+		al.addAll(aal); // will add at last
+		System.out.println(al);
+		
+		nal.addAll(2, aal); //will add from given position
+		System.out.println(nal);
 	}
 
 }
